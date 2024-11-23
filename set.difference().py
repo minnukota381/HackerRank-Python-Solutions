60022c6a3ca5f097353cb61698b704215699d89d
minnukota381 <minnukota381@gmail.com>
2024-11-23 10:03:59 +0530

    set.difference().py

---
+++ set.difference().py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+Q=int(input())
+Qu=set(map(int,input().split()))
+R=int(input())
+Ru=set(map(int,input().split()))
+
+print(len(Qu.difference(Ru)))
